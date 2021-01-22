e32ed9ef5aa1a63eb2c5e24cc12e24b4fc3849e3
Simon Peters <simonpeters05@gmail.com>
2021-01-22 20:46:55 +0100

    :zap: Base mining system done

--- Currency.py
+++ Currency.py
@@ -1,10 +1,28 @@
 import time
+import random
+import hashlib
 
 class Block:
-    def __init__(self, is_genesis = False, prev_hash = None):
-        self.isGenesis = is_genesis
+    def __init__(self, is_genesis = False, prev_hash = None, hash = None, proof = None):
+        self.is_genesis = is_genesis
         self.prev_hash = prev_hash
         self.date_time = time.time()
+        self.hash = hash
+        self.proof = proof
+    
+    def proof_of_work(self, node_signature) -> bool:
+        nonse = 1
+        while True:
+            nonse += random.randint(0, 15)
+            resolvable = hashlib.sha512(f"{node_signature}{self.date_time}{nonse}".encode()).hexdigest()
+            
+            calc = str(round(nonse / (nonse*random.randint(2, 30000) - (nonse/2)) * 10000000))
+
+            if resolvable.startswith(f"{calc[random.randint(0, 2)]}{calc[random.randint(0, 2)]}{calc[random.randint(0, 2)]}") and resolvable.endswith(f"{calc[random.randint(0, 2)]}{calc[random.randint(0, 2)]}{calc[random.randint(0, 2)]}"):
+                self.hash = resolvable
+                self.proof = 0
+                return True
+        return False
 
 class BlockChain:
     def __init__(self, node_signature):
@@ -14,4 +32,13 @@ class BlockChain:
         self.transactions = []
 
     def add_genesis_block(self) -> Block:
-        return Block(is_genesis=True)
+        block = Block(is_genesis=True)
+
+        if block.proof_of_work(self.node_signature):
+            return block
+
+        raise Exception("Failed to validate proof of work")
+
+    @property
+    def last_block(self):
+        self.chain[-1]
--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3.9
 
-from Currency import Block
+from Currency import Block, BlockChain
 
 if __name__ == "__main__":
+    blockchain = BlockChain("iadbuhpfdfpaiubh")
+    print(blockchain.chain[0].hash)
